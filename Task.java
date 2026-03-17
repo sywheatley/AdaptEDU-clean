58481371402584f7a7fa405026ca3ce57c6c179c
sywheatley <sywheatley@wpi.edu>
2026-03-17 14:10:12 -0400

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,18 @@
+Task
+public class Task {
+    private String name;
+    private String description;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

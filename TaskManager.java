ec1a2f20e414b51363104f58f56f6d3af4f79a84
sammy <sywheatley@wpi.edu>
2026-04-01 12:15:13 -0400

    YAYAYAY CONNECTION!

--- TaskManager.java
+++ TaskManager.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class TaskManager {
 
     private ArrayList<Task> tasks; // List of tasks
+    private ArrayList<Event> events; // List of events
 
     public TaskManager() {
         tasks = new ArrayList<>();
+        events = new ArrayList<>();
     }
 
+    // Task methods
     public void addTask(Task task) {
         tasks.add(task);
     }
@@ -16,6 +20,24 @@ public class TaskManager {
         tasks.remove(task);
     }
 
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    // Event methods
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    public void removeEvent(Event event) {
+        events.remove(event);
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    // Additional utility methods
     public void printTasksByUrgency() {
         tasks.sort((t1, t2) -> Integer.compare(t2.getUrgency(), t1.getUrgency()));
         for (Task task : tasks) {
@@ -31,6 +53,7 @@ public class TaskManager {
     }
 
     public Task getMostUrgentTask() {
+        if (tasks.isEmpty()) return null;
         Task urgent = tasks.get(0);
         for (Task task : tasks) {
             if (task.getUrgency() > urgent.getUrgency()) {

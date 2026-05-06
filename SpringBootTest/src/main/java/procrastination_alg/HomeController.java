b2640afbfd0ef0dff6e31a177de75a576ac62928
sammy <sywheatley@wpi.edu>
2026-05-05 20:44:14 -0400

    Serve frontend at root path

---
+++ SpringBootTest/src/main/java/procrastination_alg/HomeController.java
@@ -0,0 +1,13 @@
+package procrastination_alg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index2.html";
+    }
+}
